f12cb79b36bc7d8d1b345c6dd885c3893600d9dc
lcube <info@lcube.fr>
2023-12-17 22:47:20 +0100

    fix: pipeline

--- settings/settings.dev.php
+++ settings/settings.dev.php
@@ -30,7 +30,7 @@ $databases['default']['default'] = [
   'host' => getenv('DB_HOSTNAME'),
   'port' => getenv('DB_PORT'),
   'namespace' => getenv('DB_NAMESPACE'),
-  'driver' => 'mysql',
+  'driver' => getenv('DB_DRIVER'),
 ];
 
 # ================================================================
